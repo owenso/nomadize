f84521ddc7d767e85a9683a291311b84dc6c2e92
Owens O'Brien <owensobrien@gmail.com>
2016-01-31 16:33:20 -0500

    install lodash and autosuggest module, move location check to react, experiment sending value to searchbox

--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBox from './components/searchBox';
 
+class App extends Component {
 
-ReactDOM.render(<SearchBox/>, document.getElementById('container'));
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      iata: null,
+    };
+
+    this.getLoc();
+  }
+
+
+
+  getLoc() {
+    const _this = this;
+    var xhr = new XMLHttpRequest();
+
+  	xhr.onreadystatechange = function() {
+  	    if (xhr.readyState == XMLHttpRequest.DONE) {
+            const locResult = JSON.parse(xhr.responseText);
+  	        console.log(locResult);
+            _this.setState({iata: locResult.iata});
+  	    }
+  	};
+  	xhr.open("GET", "/api/v1/locate", true);
+  	xhr.send(null);
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBox iata = {this.state.iata} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
